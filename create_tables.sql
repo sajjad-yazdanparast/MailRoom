40c3dc44ff0cf45e1026f52de10df9cb3751c3e3
sajjad <sajjad.yazdanparast.tehrani@gmail.com>
2020-06-30 15:48:14 +0430

    till this step i created Interactor TABLE and TODO file

--- create_tables.sql
+++ create_tables.sql
@@ -1,18 +1,45 @@
 CREATE TABLE Employee (
-    ID INTEGER IDENTITY(1,1) PRIMARY KEY ,
+    ID INTEGER IDENTITY(1,1) ,  -- auto increment primary key
     name NVARCHAR(32) NOT NULL,
-    personel_number NUMERIC(10,0) NOT NULL 
+    personel_number NUMERIC(10,0) NOT NULL  ,
+
+    PRIMARY KEY (ID) 
 ) ;
 
 CREATE TABLE Dignity (
-    ID INTEGER IDENTITY(1,1) PRIMARY KEY ,
-    employee_id INTEGER NOT NULL ,
+    ID INTEGER IDENTITY(1,1)  ,  -- auto increment primary key
+    employee_id INTEGER NOT NULL ,          -- foreign key to emplyee
     name NVARCHAR(32) NOT NULL ,
     level INTEGER NOT NULL ,
-    CONSTRAINT fk_employee FOREIGN KEY (employee_id) REFERENCES Employee (ID)
+
+    PRIMARY KEY (ID) ,
+    FOREIGN KEY (employee_id) REFERENCES Employee (ID)
 ) ;
 
 CREATE TABLE Organization (
-    ID INTEGER IDENTITY(1,1) PRIMARY KEY ,
-    name NVARCHAR(64) NOT NULL 
-)
+    ID INTEGER IDENTITY(1,1) ,
+    name NVARCHAR(64) NOT NULL ,
+
+    PRIMARY KEY (ID) 
+) ;
+
+CREATE TABLE Interactor (
+    ID INTEGER IDENTITY(1,1) ,  -- auto increment primary key 
+    name NVARCHAR(64) NOT NULL ,
+    address NVARCHAR(256) NOT NULL ,
+    telephone NUMERIC(11,0) NOT NULL,   
+    postal_code NUMERIC(11,0) NOT NULL ,
+
+    employee_id INTEGER ,
+    organ_id INTEGER ,
+
+    PRIMARY KEY (ID) ,
+    FOREIGN KEY (employee_id) REFERENCES Employee(ID) ,
+    FOREIGN KEY (organ_id) REFERENCES Organization(ID) ,
+
+    CONSTRAINT CK_emp_or_org CHECK (
+        CASE WHEN employee_id IS NULL THEN 0 ELSE 1 END +
+      CASE WHEN organ_id  IS NULL THEN 0 ELSE 1 END = 1
+    ) ,
+
+);
