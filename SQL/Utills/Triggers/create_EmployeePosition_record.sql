8f28fecde625c7b9846ada8b93d999b1bafa7833
sajjad-yazdanparast <61648884+sajjad-yazdanparast@users.noreply.github.com>
2020-07-07 18:42:56 +0430

    update EmployeePosition_treigger

--- SQL/Utills/Triggers/create_EmployeePosition_record.sql
+++ SQL/Utills/Triggers/create_EmployeePosition_record.sql
@@ -15,35 +15,3 @@ BEGIN
 
 
 END 
-
--- CREATE FUNCTION employee_position_creation_signal (
---     @employee_id INTEGER ,
---     @level FLOAT
--- ) RETURNS NVARCHAR(5) as 
--- BEGIN
-
---     DECLARE @boss_level FLOAT ;
-
---     IF @level >= 3   -- if this emp is not modir amel or ozve heat modire
---     BEGIN
---         SELECT @boss_level = MIN(e2.rank) FROM
---         Employee e1 INNER JOIN Employee e2 
---         ON e1.boss_id = e2.ID
---         WHERE e1.ID = @employee_id
-
---         IF @boss_level <= @level
---         BEGIN
---             INSERT INTO EmployeePosition (employee_id,rank)  VALUES
---             (@employee_id,@level) ;
---             RETURN 'TRUE'
---         END
---         RETURN 'FALSE' ;
---     END
---     -- this emp is modir amel or ozve heiat modire
---     INSERT INTO EmployeePosition (employee_id,rank)  VALUES
---     (@employee_id,@level) ;
---     RETURN 'TRUE' ;
-
--- END 
-
-
