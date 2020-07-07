bc2d218ce8dcd709929daaa8e9ebea5791f55c81
sajjad <sajjad.yazdanparast.tehrani@gmail.com>
2020-07-07 11:32:11 +0430

    add sql files to SQL folder

--- SQL/inserting.sql
+++ SQL/inserting.sql
@@ -2,7 +2,7 @@ INSERT INTO Organization VALUES
 ('Isfahan University')
 
 INSERT INTO Interactor (name,address,telephone,postal_code,organ_id)
-VALUES ('Isfahan University','hezar herib',09136477484,09123456789,1)
+VALUES ('Isfahan University','hezar jerib',09136477484,09123456789,1)
 
 
 select * from INteractor
